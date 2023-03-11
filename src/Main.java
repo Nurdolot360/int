3457a524bc4ea6169db406e3c79053eb24fe2df9
nurdolotmamasabirov <nurdolotmamasabirov225@gmail.com>
2023-03-11 15:30:18 +0600

    Nurdolot commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(" hello Zarina эже");
         System.out.println("привет");
+        System.out.println("calamaleikum");
+
     }
 }
